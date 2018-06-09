cfdf51ecc00eca1b91d89869fbe7a8272ddc225a
Victor Santana <vsantana@microsoft.com>
2018-06-09 10:46:13 -0500

    test tag

--- PowerShell - Web JSon Request.ps1
+++ PowerShell - Web JSon Request.ps1
@@ -25,3 +25,5 @@ $JSON = @'
 '@
 
 $response = Invoke-RestMethod -Uri "https://<FQDN or IP>/api/jsonpost" -Method Post -Body $JSON -ContentType "application/json" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+
+#test tag
